cd1eac52526ef5204cd21ff2405f14bb155ac7c1
Philipp Wahle <phil.wahle@student.htw-berlin.de>
2016-03-08 21:23:54 +0100

    Update the example files

--- example/app.js
+++ example/app.js
@@ -1,5 +1,7 @@
 (function () {
 
+    'use strict';
+
     angular
         .module('app', [
             'ngAria',
@@ -14,9 +16,11 @@
     function AppConfig($uiContainerProvider) {
 
         $uiContainerProvider.setUiFlex(20);
+        $uiContainerProvider.setUiShowOn('gt-sm');
     }
 
     function AppCtrl() {
 
     }
+
 })();
